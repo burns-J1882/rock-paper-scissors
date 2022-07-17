56de7eeab01f28f455414f07f37d435c8f0fb471
James Burns <burns.j1882@gmail.com>
2022-07-17 16:03:08 -0400

    Setup the project files Pseudo code app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello world')
+
+/*
+function getComputerChoice to randomly pick rock paper or scissors
+function playerSelection to select rock paper or scissors (make case insensitive)
+function playRound that compares choices
+returns win or loss for the player
+make a loop to play 5 rounds
+*/
