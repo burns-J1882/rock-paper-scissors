9f64e4653249a4253a6455d6aac1b3953af8141d
James Burns <burns.j1882@gmail.com>
2022-08-04 22:05:08 -0400

    Make game button based and output score to DOM

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rock-paper-scissors
 A turn based rock, paper, scissors game versus the computer.
+
+08-01-2022
+    The original game was played by initiating the game() function in the console. A player would then enter their choice in a prompt and the results were again returned in the console
+
+    Today begins the creation of a UI for the game
+08-04-2022
+    A super a basic UI that works!
+    The functions are initiated by a click on the button that returns a new paragraph with the scores embedded. 
+    Looking at it now i'd probably try and work some more named functions to call on the clicks arrow function
+    as this is pretty hacky and I can't get the thing to automatically display game over until a user tries to
+    play a 5th round. Learned a lot about querySelectors and events in the DOM so right now I'm just happy the
+    thing works. a refactor is definitely a good idea to clean up the code, and maybe try some different loops.
--- app.js
+++ app.js
@@ -1,50 +1,75 @@
-console.log('hello world')
+//player clicks button - info is stored
+//event listener, calls playRound w/ correct play selection
+//computer selects random choice
+//compare the two (return win loss or draw)
+//output the users choice to the DOM
+//output computers random choice to the DOM
+//return the winner to the DOM
+//essentially turn the console.log/s to DOM methods
 
-/*
-function getComputerChoice to randomly pick rock paper or scissors
-function playerSelection to select rock paper or scissors (make case insensitive)
-function playRound that compares choices
-returns win or loss for the player
-make a loop to play 5 rounds
-*/
-
-let choice = ['rock', 'paper', 'scissors']
-//testing to track wins and losses
+const choices = [ 'rock', 'paper', 'scissors' ];
 let win = 0;
 let loss = 0;
 let draw = 0;
 
+
 function computerChoice() {
-    return choice[Math.floor(Math.random() * choice.length)];
+	return choices[Math.floor(Math.random() * choices.length)];
 }
+const buttons = document.querySelectorAll('button'); //this makes the node list of all buttons
+//forEach method iterates the function to be applied over each button
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		let user = button.id;
+		let computer = computerChoice();
+        let round = (win+loss+draw);//the counter intializes at 0
+		if (user == computer && round < 5) {
+			draw++;
+			result = `its a tie, the score is W:${win}, D:${draw}, L:${loss}`;
+			const output = document.createElement('p');
+			output.classList.add('results');
+			output.textContent = result;
+			container.appendChild(output);
+            console.log(round);
+		} else if (
+			(user == 'rock' && computer == 'scissors' && round < 5) ||
+			(user == 'paper' && computer == 'rock' && round < 5) ||
+			(user == 'scissors' && computer == 'rock' && round < 5)
+		) {
+			win++;
+			result = `${user} beats ${computer}, the score is W:${win}, D:${draw}, L:${loss}`;
+			const output = document.createElement('p');
+			output.classList.add('results');
+			output.textContent = result;
+			container.appendChild(output);
+            console.log(round);
+			//console.log(`${user} beats ${computer} you win!`);
+		} else if (round < 5){
+			loss++;
+            result = `${computer} beats ${user},the score is W:${win}, D:${draw}, L:${loss}`;
+            const output = document.createElement('p');
+			output.classList.add('results');
+			output.textContent = result;
+			container.appendChild(output);
+            console.log(round);
+			//console.log(`${computer} beats ${user}, You lose!`);
+		} else {
+            console.log('GAME OVER')
+            const endGame = document.createElement('p');
+            endGame.classList.add('fs');
+            endGame.textContent = `GAME OVER - Your final scores are W:${win}, D:${draw}, L:${loss}`;
+            container.appendChild(endGame);
+        }
+	});
+});
 
-function playerSelection() {
-    let input = prompt('Please type one of rock, paper or scissors');
-    return input.toLowerCase();
-}
+const container = document.querySelector('.container');
 
-function playRound () {
-    let user = playerSelection();
-    let computer = computerChoice();
-    if (user == computer) {
-        console.log(`You both played ${user}, its a tie!`);
-        //add one to draw
-        draw++;
-    } else if (user == 'rock' && computer == 'scissors' || user == 'paper' && computer == 'rock' || user == 'scissors' && computer == 'rock'){
-        console.log(`${user} beats ${computer} you win!`);
-        //add one to win
-        win++
-    } else {
-        console.log(`${computer} beats ${user}, You lose!`)
-        //add one to loss
-        loss++;
-    }
-}
 
-function game() {
-    for (let i = 0; i < 5; i++){
-        playRound();
-    }
-    //logging win lose draws
-    console.log(`your final score is ${win} wins, ${loss} losses, and ${draw} draws`)
-}
+/* example of a DOM adjustment
+result = 'its a tie';
+const output = document.createElement('p');
+output.classList.add('results');
+output.textContent = result;
+container.appendChild(output);
+*/
